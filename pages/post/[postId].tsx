a4e301684bd8791fbcd4ce35c47332cc8e01589a
rokkhi-shahin <ahmshahin5353@gmail.com>
2022-11-16 12:53:20 +0600

    getServerSideProps getStaticPaths getStaticProps tailwindcss configure fetch from api async await

---
+++ pages/post/[postId].tsx
@@ -0,0 +1,41 @@
+import Link from 'next/link';
+import React from 'react'
+
+export default function Post({ post }:any) {
+    return (
+        <div style={{ textAlign: 'center' }}>
+            <h1 className="text-4xl my-4">Post</h1>
+            <div>
+                <h1 className="text-4xl">{post.id} {post.title}</h1>
+                <p className="text-2xl my-10">{post.body}</p>
+            </div>
+        </div>
+
+    )
+}
+
+export async function getStaticPaths() {
+    let posts: any = await fetch('https://jsonplaceholder.typicode.com/posts')
+        .then((response) => response.json())
+        .then((data) => data);
+    const paths = posts.map((post: any) => ({
+        params: { postId: post.id.toString() },
+    })).flat()
+    console.log("paths");
+
+    return {
+        paths,
+        fallback: false
+    }
+}
+export async function getStaticProps(context:any) {
+    let { params } = context;
+    let post = await fetch(`https://jsonplaceholder.typicode.com/posts/${params.postId}`)
+        .then((response) => response.json())
+        .then((data) => data);
+    console.log(post.title);
+    return {
+        // Passed to the page component as props
+        props: { post },
+    }
+}
---
+++ pages/post/index.tsx
@@ -0,0 +1,34 @@
+import Link from 'next/link';
+import React from 'react'
+
+export default function Posts({posts}:any) {
+    return (
+        <div style={{ textAlign: 'center' }}>
+            <h1 className="text-4xl my-4">Posts</h1>
+            <div>
+            <ul className="w-full text-sm font-medium text-gray-900 rounded-lg border border-gray-200">
+            {
+                posts.map((post:any)=>
+                <Link href={`/post/${post.id}`} key={post.id}>
+                <li  className="py-2 px-4 w-full border-b border-gray-200 dark:border-gray-600">
+                    {post.title}
+                </li>
+                </Link>
+                )
+            }
+            </ul>
+
+            </div>
+        </div>
+        
+    )
+}
+
+export async function getServerSideProps() {
+    let posts:any = await fetch('https://jsonplaceholder.typicode.com/posts')
+        .then((response) => response.json())
+        .then((data) => data);
+    return {
+        props: { posts }
+    }
+}
