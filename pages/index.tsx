7c26dc9bed58e97267844eb9074874df2ebc016c
rokkhi-shahin <ahmshahin5353@gmail.com>
2022-11-16 16:05:53 +0600

    next Image optimization testing

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,29 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import CodeSnippet from '../public/code-snippet.png'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className="py-0 px-8">
       <Head>
         <title>Hunting Coder</title>
         <meta name="description" content="Coder Blog" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
+      <main className="min-h-screen px-16 flex-1 flex flex-col justify-center items-center">
+        <h1 className="leading-5 text-[4rem]">
           Hunting Coder
         </h1>
-
-        <p className={styles.description}>
-         A blog for hunting coders by a hunting coder
+        <Image placeholder="blur" className='my-10 border rounded-lg' height={300} width={600} src={CodeSnippet} alt="code-snippet" />
+        <p className="text-2xl">
+          A blog for hunting coders by a hunting coder
         </p>
 
       </main>
 
-      <footer className={styles.footer}>
-          Developed by<span style={{fontWeight:'bold', marginLeft:'5px'}}>Shahin</span>
+      <footer className="flex flex-1 justify-center items-center border-t py-8">
+        Developed by<span style={{ fontWeight: 'bold', marginLeft: '5px' }}>Shahin</span>
       </footer>
     </div>
   )
